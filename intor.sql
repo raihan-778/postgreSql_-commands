9d4433674373938649e7f0ab1a222f6ed5952282
Raihan Uddin <raihanarif778@gmail.com>
2023-09-06 12:41:17 +0600

    innitial commit

---
+++ intor.sql
@@ -0,0 +1,68 @@
+-- Active: 1693892583935@@127.0.0.1@5432@test_01
+--## Create A students TABLE
+-- create TABLE students (
+--  student_id INTEGER,
+-- firsr_name VARCHAR(50),
+-- last_name VARCHAR(50),
+-- cgpa NUMERIC(1,2)
+-- )
+
+--## Rename a Tabel
+
+ALTER TABLE students RENAME to learnears
+
+--## Delete a Tabel
+-- DROP TABLE learnears
+
+--## Create a TABLE
+-- create TABLE "user"(
+-- user_id SERIAL PRIMARY KEY,
+-- user_name VARCHAR(255) UNIQUE NOT NULL,
+-- email VARCHAR(255) UNIQUE NOT NULL
+-- )
+create TABLE "user2"(
+user_id SERIAL,
+user_name VARCHAR(255) NOT NULL UNIQUE,
+email VARCHAR(255) NOT NULL UNIQUE,
+age int DEFAULT 18
+)
+
+insert into user2 values (1, 'abc', 'ab@gmail.com')
+
+--multiple insert in a TABLE
+insert into user1 values
+(1,'raihan', 'raihan@gmail.com'),
+(2, 'rajib', 'rajib@gmail.com');
+
+--### Alter TABLE
+-- Add a column, drop a column, change datatype in a COLUMN
+-- rename a column, set default value for a COLUMN
+-- add constrain to a column, drop CONSTRAINT for a COLUMN
+-- table RENAME
+-- command line -> (alter table table_name action(COMMAND FOR ADD RENAME DROP ETC))
+--## example
+
+--## add a column
+alter table user1 
+add column password VARCHAR(255) default 'admin123' not null
+
+alter table user1 
+add COLUMN age INT DEFAULT 23 not null;
+
+--## delete a column
+alter table user1
+drop COLUMN age 
+--## add a column
+alter table user1
+add COLUMN demu INT
+
+--## change data type of a column
+alter table user1
+ alter COLUMN demu type text;
+
+ --## set default value in a COLUMN
+ alter TABLE user1
+  alter column demu set default 'add_text';
+
+  --## insert values in a table
+  insert into user1 values(1, 'demu_1', 'demu_1');
